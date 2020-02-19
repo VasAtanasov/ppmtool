72a243cdfd3f1170048da4db2cd6b27be331510c
VasAtanasov <vas.atanasov@gmail.com>
2020-02-19 14:33:51 +0200

    init commit

---
+++ ppmtool-server/src/test/java/org/tools/ppmtool/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tools.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
